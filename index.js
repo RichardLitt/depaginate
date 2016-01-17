c79a37bfc5cbeb23b4cf607fbeab98060b544f74
Richard Littauer <richard.littauer@gmail.com>
2016-01-17 22:08:59 +0100

    Refactor

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const arr = []
 module.exports = function loadAllPages (callFx, opts) {
   opts['page'] = opts.page || 1
 
-  return Promise.resolve().then(function () {
-    return callFx(opts)
-  }).then(function (result) {
-    arr.push(result)
-    if (_.isFunction(result.nextPage)) {
-      opts.page = opts.page + 1
-      return loadAllPages(callFx, opts)
-    } else {
-      return Promise.resolve(_.flatten(arr))
-    }
-  }).catch(function (err) {
-    if (err) {
-      console.log('Failed to depaginate\n', err)
-    }
+  return Promise
+    .resolve(callFx(opts))
+    .then((result) => {
+      arr.push(result)
+      if (_.isFunction(result.nextPage)) {
+        opts.page = opts.page + 1
+        return loadAllPages(callFx, opts)
+      } else {
+        return Promise.resolve(_.flatten(arr))
+      }
+    }).catch((err) => {
+      if (err) {
+        console.log('Failed to depaginate\n', err)
+      }
   })
 }
